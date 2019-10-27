b4df0720def54e7f9cbc8142ad0bfd7c0677024a
Nursyafira Jamil <32829952+fyra-jamil@users.noreply.github.com>
2019-10-27 18:13:15 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import 'react-native';
+import React,{ Component } from 'react';
+import Main from './app/components/Main';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+
+    );
+  }
+}
